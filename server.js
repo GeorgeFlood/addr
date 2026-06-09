657c8774252fbee9e4b86b092860b7f41185058f
George Flood <george@Georges-MacBook.local>
2026-06-09 13:46:44 +0100

    added a rate limiter

--- server.js
+++ server.js
@@ -4,12 +4,20 @@ import lookupRouter from './addressRoutes.js';
 const PORT = Number(process.env.PORT) || 3000;
 import path from 'path';
 import { fileURLToPath } from 'url';
+import rateLimit from 'express-rate-limit';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const limiter = rateLimit({
+  windowMs: 24 * 60 * 60 * 1000,
+  max: 50,
+  message: 'Reached the API daily limit',
+});
+
 const app = express();
 
 app.use(express.static(__dirname));
+app.use('/v1/address/', limiter);
 app.use(lookupRouter);
 
 app.listen(`${PORT}`, () => {
